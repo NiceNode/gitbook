d446b01c73dc9e4b7c97291e9bf19c61ee290f1c
jgresham <johnsgresham@gmail.com>
2023-11-27 15:32:48 -0800

    fix: update prod url to docs.nicenode.xyz

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   favicon: 'img/favicon.png',
 
   // Set the production url of your site here
-  url: 'https://nicenode.xyz',
+  url: 'https://docs.nicenode.xyz',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs',
+  baseUrl: '/',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
